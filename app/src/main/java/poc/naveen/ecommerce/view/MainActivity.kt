deca8e9b835edeb748b207159eea529fd508bb52
Naveen Prince <naveenprincep@gmail.com>
2021-02-10 13:18:01 +0530

    changes

--- app/src/main/java/poc/naveen/ecommerce/view/MainActivity.kt
+++ app/src/main/java/poc/naveen/ecommerce/view/MainActivity.kt
@@ -1,12 +1,8 @@
 package poc.naveen.ecommerce.view
 
 import android.os.Bundle
-import android.view.ViewGroup
-import android.widget.FrameLayout
-import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import poc.naveen.ecommerce.R
 import poc.naveen.ecommerce.databinding.ActivityMainBinding
@@ -34,44 +30,20 @@ class MainActivity : AppCompatActivity() {
     private fun fetchWidgetDetails() {
         widget = MainRepository().fetchWidgetDetails(this)
         sortedList = widget.widgetDetails.sortedWith(compareBy { it.orderNumber })
-        setWidgetLoadingDetails()
-
-    }
-
-    private fun setWidgetLoadingDetails() {
-        val layout: List<Int> = listOf(R.id.container_top, R.id.container_center, R.id.container_bottom)
-        var i = 0;
-        for (item in sortedList) {
-            item.layoutId = layout[i++]
-        }
     }
 
     private fun loadFragments() {
+        val layout: List<Int> = listOf(R.id.container_top, R.id.container_center, R.id.container_bottom)
+        var i = 0;
         val fragmentTransaction: FragmentTransaction = supportFragmentManager.beginTransaction()
         for (item in sortedList) {
             when (item.widgetId) {
-                "1" -> {
-                    fragmentTransaction.replace(item.layoutId, ProductCarouselFragment.newInstance())
-                    setLayoutWeight(item.layoutId, 15.0f)
-                }
-                "2" -> {
-                    fragmentTransaction.replace(item.layoutId, ProductCollectionFragment.newInstance())
-                    setLayoutWeight(item.layoutId, 60.0f)
-                }
-                "3" -> {
-                    fragmentTransaction.replace(item.layoutId, BannerImageFragment.newInstance())
-                    setLayoutWeight(item.layoutId, 25.0f)
-                }
+                "1" -> fragmentTransaction.replace(layout[i++], ProductCarouselFragment.newInstance())
+                "2" -> fragmentTransaction.replace(layout[i++], ProductCollectionFragment.newInstance())
+                "3" -> fragmentTransaction.replace(layout[i++], BannerImageFragment.newInstance())
+
             }
         }
         fragmentTransaction.commit()
-    }
-
-    private fun setLayoutWeight(layoutId: Int, weight: Float) {
-        val frameLayout: FrameLayout = findViewById(layoutId)
-        val params: LinearLayout.LayoutParams =
-            LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 0)
-        params.weight = weight
-        frameLayout.layoutParams = params
     }
 }
